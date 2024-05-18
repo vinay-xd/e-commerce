94a86e4e7db99bf790ffb5658bfca2f0bb619acc
01MANSI <senmansi2001@gmail.com>
2024-05-18 17:08:38 +0530

    add to kart

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,14 +1,68 @@
 import React from 'react'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar';
-import Topbar from './Components/Topbar/Topbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/UserDetails/Login.jsx'
 import Signup from './Components/UserDetails/Signup.jsx'
+import Wishlist from './Components/SaveProduct/Wishlist.jsx';
+import Topbar from './Components/Topbar/Topbar';
+import * as Images from '../src/assets/Images/img.js'
 
 function App() {
 
-
+  const data=[
+    {
+        id:1,
+        imgUrl: Images.Bag
+        // description:{}
+    },
+    {
+      id:2,
+      imgUrl:Images.Jacket
+      // description:{}
+  },
+  {
+    id:3,
+    imgUrl:Images.Eye
+    // description:{}
+},
+{
+  id:4,
+  imgUrl:Images.Game
+  // description:{}
+},
+{
+  id:5,
+  imgUrl:'C:\Users\acer\Desktop\Group\e-commerce\Front-End\src\assets\Images\Bag.png'
+  // description:{}
+},
+,
+{
+  id:1,
+  imgUrl: Images.Bag
+  // description:{}
+},
+{
+id:2,
+imgUrl:'e-commerce\Front-End\src\assets\Images\Bag.png'
+// description:{}
+},
+{
+id:3,
+imgUrl:'C:\Users\acer\Desktop\Group\e-commerce\Front-End\src\assets\Images\Bag.png'
+// description:{}
+},
+{
+id:4,
+imgUrl:'C:\Users\acer\Desktop\Group\e-commerce\Front-End\src\assets\Images\Bag.png'
+// description:{}
+},
+{
+id:5,
+imgUrl:'C:\Users\acer\Desktop\Group\e-commerce\Front-End\src\assets\Images\Bag.png'
+// description:{}
+}
+]
   return (
     <>
 
@@ -16,11 +70,14 @@ function App() {
       <Router>
       <Topbar />
       <Navbar />
+       {/* <Wishlist data={data} /> */}
         <Routes>
+          /
           {/* <Route path='/' element={<Navbar />} />
           <Route path='/' element={<Topbar />} /> */}
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
+          <Route path='/wishlist' element={<Wishlist data={data} />}  />
         </Routes>
 
       </Router>
--- Front-End/src/assets/Images/img.js
+++ Front-End/src/assets/Images/img.js
@@ -1,2 +1,7 @@
 export {default as Login} from './Login.jpg'
 export {default as Hero} from './hero.jpeg'
+export {default as Bag} from './Bag.png'
+export {default as Jacket} from './jacket.png'
+export {default as Game} from './game.png'
+export {default as Eye} from './eye.png'
+
